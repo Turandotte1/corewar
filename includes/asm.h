59f3d17817f24caba9c4c0cd1f6dea56a9974210
Minh PHAM <mipham@student.42.fr>
2018-05-07 13:30:50 +0200

    ajour Header

--- includes/asm.h
+++ includes/asm.h
@@ -6,20 +6,16 @@
 /*   By: mipham <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 19:38:27 by mipham            #+#    #+#             */
-/*   Updated: 2018/04/27 19:40:28 by mipham           ###   ########.fr       */
+/*   Updated: 2018/05/07 13:28:44 by mipham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by Minh PHAM on 4/19/18.
-//
-
 #ifndef ASM_H
 #define ASM_H
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include "../libft/libft_all.h"
 # include "op.h"
 # include "asm_struct.h"
