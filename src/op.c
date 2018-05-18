8f2e57526663939e6120cc4aa2867af94322dc8e
Minh PHAM <mipham@student.42.fr>
2018-05-18 17:42:37 +0200

    norme presque fini

--- src/op.c
+++ src/op.c
@@ -10,12 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/asm.h"
 /*
 ** nom, nombre de parametre, tableau de type, operateur code, nb_cycles,
 ** display name,
 ** OPC, nombre d'octet parametre direct : 0 cad 2 octets 1 cad 4 octets
 */
+#include "../includes/asm.h"
+
 t_op		g_op_tab[17] =
 {
 	{0, 0, {0}, 0, 0, 0, 0, 0},
