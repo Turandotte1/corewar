f4b3b96c0ab9d2bf3a96e5d36b4b8830d46f866e
Minh PHAM <mipham@student.42.fr>
2018-06-29 15:58:05 +0200

    ajout script test

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 for filename in champ/*.s; do
-		./asm "$filename"
-	done
-	for filename in champ/*.cor; do
-			hexdump -c "$filename" > "$filename".txt 
-		done
-		echo "-------------------------------------------------\n"
-		for filename in champ/*.s; do
-				./asmzaz "$filename"
-			done
-			for filename in champ/*.cor; do
-					hexdump -c "$filename" > "$filename"2.txt 
-				done
-				for filename in champ/*.cor; do
-						diff "$filename".txt "$filename"2.txt 
-					done
+	./asm "$filename"
+done
+for filename in champ/*.cor; do
+	hexdump -c "$filename" > "$filename".txt 
+done
+echo "-------------------------------------------------\n"
+for filename in champ/*.s; do
+	./asmzaz "$filename"
+done
+for filename in champ/*.cor; do
+	hexdump -c "$filename" > "$filename"2.txt 
+done
+for filename in champ/*.cor; do
+	diff "$filename".txt "$filename"2.txt 
+done
